15115be19d0cd459b260b358dd5494bef84e0816
Mr.K <roseboy@live.com>
2023-06-30 22:32:18 +0800

    feat:action demo

--- demo/action_demo/main.go
+++ demo/action_demo/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
-	"time"
-
 	"github.com/roseboy/go-ng/ng"
 	"github.com/roseboy/go-ng/plugin"
 	"github.com/roseboy/go-ng/plugin/action"
+	"time"
 )
 
 func main() {
+	printTestInfo()
+
+	plg := plugin.NewActionPlugin("/api", true, GetAuthInfo)
+	plg.RegisterAction(GetGirlfriend, &GetGirlfriendRequest{}, &GetGirlfriendResponse{})
+
+	err := ng.NewServer(8000).RegisterPlugins(plg).Start()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func printTestInfo() {
 	timestamp := time.Now().Unix() + 5
 	body := fmt.Sprintf(`{"Action":"GetGirlfriend","Age":20,"Timestamp":%d,"AppId":111}`, timestamp)
 	sign := action.CalcSignatureV1(&action.SignatureArgs{
@@ -20,12 +31,4 @@ func main() {
 	fmt.Println(" execute the following command in 10s:")
 	fmt.Printf(" curl 'http://localhost:8000/api?a=1' -d'%s' -H'Authorization:%s'\n", body, authorization)
 	fmt.Println()
-
-	plg := plugin.NewActionPlugin("/api", true, GetAuthInfo)
-	plg.RegisterAction(GetGirlfriend, &GetGirlfriendRequest{}, &GetGirlfriendResponse{})
-
-	err := ng.NewServer(8000).RegisterPlugins(plg).Start()
-	if err != nil {
-		panic(err)
-	}
 }
